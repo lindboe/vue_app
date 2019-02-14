701471047e22ed4f63d97fbfd8197d3d3c242aef
lindboe <lindboe@users.noreply.github.com>
2019-02-13 22:37:23 -0800

    What it looks like to turn the box and button into a root-level component
    Definitely not simpler for something like this, but learned about
    components.
    
    Learned:
    - for multi-line template, can inline with backticks, but really would
    want to use another way on more complex components, maybe single file
    components (see
    https://vuejsdevelopers.com/2017/03/24/vue-js-component-templates/)
    - v-model breaks down into `v-bind:value="databind" v-on:input="databind = $event.target.value"` so any child component gets bassed the `value` prop
    
    Question: is it okay, or bad, or even allowed, to have the component
    persist to localstorage directly? react would say it's bad.

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 "use strict";
 
+Vue.component('text-box', {
+  props: ['value'],
+  template:
+    `<div class="text-box">
+       <input
+         v-bind:value="value"
+         v-on:input="$emit('input', $event.target.value)">
+       <button @click="$emit('persist')">Save</button>
+     </div>`
+})
+
+var data = {
+  text: "This is a text box."
+}
+
 var page = new Vue({
   el: "#page",
-  data: {
-    text: "This is a text box."
-  },
+  data: data,
   mounted() {
     if (localStorage.text) {
       this.text = localStorage.text;
